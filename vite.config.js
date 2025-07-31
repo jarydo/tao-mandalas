a214b1a307c751e694f3684c7c1d52694f9ec5c0
jarydo <jarydnoahdiamond@gmail.com>
2025-07-31 01:39:40 -0700

    add vite legacy plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "https://tao-mandalas-jaryd-diamonds-projects.vercel.app/",
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
 });
